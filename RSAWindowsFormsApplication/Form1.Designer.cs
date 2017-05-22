974d6ef7154434b94878bd42981f406f7e1d50fe
Lin Kuan Yu <idomywant@hotmail.com>
2017-05-22 17:24:49 +0800

    Commit for init

---
+++ RSAWindowsFormsApplication/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace RSAWindowsFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBoxOrigional = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypt = new System.Windows.Forms.TextBox();
+            this.textBoxDecrypt = new System.Windows.Forms.TextBox();
+            this.textBoxKeyInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "GenKeyPair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBoxOrigional
+            // 
+            this.textBoxOrigional.Location = new System.Drawing.Point(32, 165);
+            this.textBoxOrigional.Name = "textBoxOrigional";
+            this.textBoxOrigional.Size = new System.Drawing.Size(306, 22);
+            this.textBoxOrigional.TabIndex = 3;
+            // 
+            // textBoxEncrypt
+            // 
+            this.textBoxEncrypt.Location = new System.Drawing.Point(32, 220);
+            this.textBoxEncrypt.Multiline = true;
+            this.textBoxEncrypt.Name = "textBoxEncrypt";
+            this.textBoxEncrypt.Size = new System.Drawing.Size(306, 201);
+            this.textBoxEncrypt.TabIndex = 4;
+            // 
+            // textBoxDecrypt
+            // 
+            this.textBoxDecrypt.Location = new System.Drawing.Point(32, 466);
+            this.textBoxDecrypt.Name = "textBoxDecrypt";
+            this.textBoxDecrypt.Size = new System.Drawing.Size(306, 22);
+            this.textBoxDecrypt.TabIndex = 5;
+            // 
+            // textBoxKeyInfo
+            // 
+            this.textBoxKeyInfo.Location = new System.Drawing.Point(413, 74);
+            this.textBoxKeyInfo.Multiline = true;
+            this.textBoxKeyInfo.Name = "textBoxKeyInfo";
+            this.textBoxKeyInfo.Size = new System.Drawing.Size(408, 414);
+            this.textBoxKeyInfo.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 631);
+            this.Controls.Add(this.textBoxKeyInfo);
+            this.Controls.Add(this.textBoxDecrypt);
+            this.Controls.Add(this.textBoxEncrypt);
+            this.Controls.Add(this.textBoxOrigional);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBoxOrigional;
+        private System.Windows.Forms.TextBox textBoxEncrypt;
+        private System.Windows.Forms.TextBox textBoxDecrypt;
+        private System.Windows.Forms.TextBox textBoxKeyInfo;
+    }
+}
+
---
+++ RSAWindowsFormsApplication/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSAWindowsFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        string strPublicKey = "";
+        string strPrivateKey = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider rsaCryptoServiceProvider = new RSACryptoServiceProvider();
+            RSAParameters rsaParameters = rsaCryptoServiceProvider.ExportParameters(true);
+            strPublicKey = rsaCryptoServiceProvider.ToXmlString(false);
+            strPrivateKey = rsaCryptoServiceProvider.ToXmlString(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string encryptString = encrypt(strPublicKey, textBoxOrigional.Text.Trim());
+
+            textBoxEncrypt.Text = encryptString;
+            textBoxKeyInfo.Text = strPublicKey;
+        }
+
+        private string encrypt(string publicKey, string context)
+        {
+            RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider();
+            rsaProvider.FromXmlString(publicKey);
+            var encryptString = Convert.ToBase64String(rsaProvider.Encrypt(Encoding.UTF8.GetBytes(context),false));
+            return encryptString;
+        }
+
+        private string decrypt(string privateKey, string context)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+            var decryptString = Encoding.UTF8.GetString(rsa.Decrypt(Convert.FromBase64String(context), false));
+            return decryptString;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxDecrypt.Text = decrypt(strPrivateKey, textBoxEncrypt.Text.Trim());
+            textBoxKeyInfo.Text = strPrivateKey;
+        }
+    }
+}
